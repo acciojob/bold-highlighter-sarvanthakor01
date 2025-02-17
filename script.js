1f793f34119520ce5ce37fcd34cac6007c8b240d
Sarvan Thakor <97446457+sarvanthakor01@users.noreply.github.com>
2025-02-17 22:50:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
-    //Write your code here
+    // Select all <strong> elements
+    let boldWords = document.querySelectorAll('strong');
 
-
+    // Loop through each and change the color to green
+    boldWords.forEach(word => {
+        word.style.color = "rgb(0, 128, 0)";
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
+    // Select all <strong> elements
+    let boldWords = document.querySelectorAll('strong');
 
-    
+    // Loop through each and change the color back to black
+    boldWords.forEach(word => {
+        word.style.color = "rgb(0, 0, 0)";
+    });
 }
